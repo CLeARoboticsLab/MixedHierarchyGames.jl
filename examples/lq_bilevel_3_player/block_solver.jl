05a552dd5498c932b18ae403859b005bbf9c53ba
Hamzah Khan <2308491+khanh111@users.noreply.github.com>
2025-05-27 13:25:01 -0500

    Implement final stage P1 solver.

--- examples/lq_bilevel_3_player/block_solver.jl
+++ examples/lq_bilevel_3_player/block_solver.jl
@@ -36,3 +36,31 @@ K3 = sol23[Block(2,1)]; # K3 is the feedback gain for player 3
 P3 = sol23[Block(2,2)]; # P3 is the feedforward gain for player 3
 # NE: u2 = K2 * x + P2 * u1
 # NE: u3 = K3 * x + P3 * u1
+
+
+"""
+Solve the Stackelberg hierarchy for the leader (P1) at the terminal stage.
+"""
+# Construct M1 as a 14x14 block matrix.
+M1 = BlockArray([
+    [R[:,:,1] Zeros2 Zeros2 Zeros2 -B[:,player_control_list[1]]'  -P2'   -P3' ];
+    [Zeros2   Zeros2 Zeros2 Zeros2 -B[:,player_control_list[2]]'  I(2)  Zeros2];
+    [Zeros2   Zeros2 Zeros2 Zeros2 -B[:,player_control_list[3]]' Zeros2  I(2) ];
+    [Zeros2   Zeros2 Zeros2 Q[:,:,1]          I(2)               Zeros2 Zeros2];
+    [-B[:,player_control_list[1]] -B[:,player_control_list[2]] -B[:,player_control_list[3]] I(2) Zeros2 Zeros2 Zeros2];
+    [-P2      I(2)   Zeros2 Zeros2 Zeros2                        Zeros2 Zeros2];
+    [-P3      Zeros2 I(2)   Zeros2 Zeros2                        Zeros2 Zeros2]
+], [2, 2, 2, 2, 2, 2, 2], [2, 2, 2, 2, 2, 2, 2])
+
+# Construct N1 as a 14x2 block matrix.
+N1 = BlockArray([
+    zeros(8, 2); # First 8 rows are zero
+    -A;
+    -K2;
+    -K3;
+], [8, 2, 2, 2], [2]) # Row blocks and column blocks
+
+sol1 = -M1 \ N1 # P1 is the NE for player 1 for the terminal stage
+K1 = sol1[Block(1, 1)]
+# NE for player 1: u1 = K1 * x
+println("first with blocks: ", K1)
