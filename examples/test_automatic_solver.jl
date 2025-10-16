4540a8c108a38189cbf70e86daa37bcc32bb7b8f
Hamzah Khan <2308491+hmzh-khn@users.noreply.github.com>
2025-10-16 15:09:09 -0500

    Refactor files to prepare for hardware refactor. (#15)
    * Add and pass a python integration test.
    
    * Refactor problem definition + reduce prints + fix evaluate calls.
    
    ---------
    
    Co-authored-by: Hamzah Khan <2308491+khanh111@users.noreply.github.com>

--- examples/test_automatic_solver.jl
+++ examples/test_automatic_solver.jl
@@ -320,7 +320,7 @@ function preoptimize_nonlq_solver(H, graph, primal_dimension_per_player, Js, gs;
 		z̅ = fill(Inf, length(F_sym))
 
 		# Form mcp via ParametricMCP initialization.
-		println(length(all_variables), " variables, ", length(F_sym), " conditions")
+		println("\n", length(all_variables), " variables, ", length(F_sym), " conditions")
 		mcp_obj = ParametricMCPs.ParametricMCP(F_sym, all_variables, all_K_syms_vec, z̲, z̅; compute_sensitivities = false)
 	end
 
@@ -403,8 +403,6 @@ function run_nonlq_solver(H, graph, primal_dimension_per_player, Js, gs, z0_gues
 	λs = problem_vars.λs
 	μs = problem_vars.μs
 	θ = problem_vars.θ
-	ws = problem_vars.ws
-	ys = problem_vars.ys
 
 	# Unpack preoptimization info - Symbolic KKT conditions and evaluation functions.
 	setup_info = preoptimization_info.setup_info
@@ -576,7 +574,7 @@ function compare_lq_and_nonlq_solver(H, graph, primal_dimension_per_player, Js, 
 	# Run our solver through the run_lq_solver call.
 	z_sol_nonlq, status_nonlq, info_nonlq, all_variables, (; πs, zs, λs, μs, θ), all_augmented_vars = run_nonlq_solver(H, graph, primal_dimension_per_player, Js, gs; tol=1e-3, parameter_value, verbose)
 	
-	println("Non-LQ solver status after $(info_nonlq.num_iterations) iterations: $(status_nonlq)")
+	verbose && println("Non-LQ solver status after $(info_nonlq.num_iterations) iterations: $(status_nonlq)")
 
 	lq_vars = setup_problem_variables(H, graph, primal_dimension_per_player, gs; verbose)
 	all_lq_variables = lq_vars.all_variables
@@ -593,17 +591,20 @@ function compare_lq_and_nonlq_solver(H, graph, primal_dimension_per_player, Js, 
 	end
 	@timeit to1 "LQ KKT Condition Solve" begin
 		z_sol_lq, status_lq = lq_game_linsolve(lq_πs, all_lq_variables, lq_θ, parameter_value; verbose)
-		z_sol_lq = nothing
-		status_lq = :not_implemented
 	end
-	show(to1)
+	info_lq = (; πs = lq_πs)
+	if verbose
+		show(to1)
+	end
 
-	# TODO: Ensure that the solutions are the same for an LQ example.
-	@assert isapprox(z_sol_nonlq, z_sol_lq, atol = 1e-4)
-	@show "Are they the same? > $(isapprox(z_sol_nonlq, z_sol_lq, atol = 1e-4))"
-	@show status_lq
+	# Ensure that the solutions are the same for the LQ solver and the non-LQ solver on an LQ example.
+	@assert isapprox(z_sol_nonlq, z_sol_lq, atol=1e-4)
+	if verbose
+		@show "Are they the same? > $(isapprox(z_sol_nonlq, z_sol_lq, atol = 1e-4))"
+		@show status_lq
+	end
 
-	z_sol_nonlq, status_nonlq, z_sol_lq, status_lq, info_nonlq, all_variables, (; πs, zs, λs, μs, θ), all_augmented_vars
+	z_sol_nonlq, status_nonlq, z_sol_lq, status_lq, info_nonlq, info_lq, all_variables, (; πs, zs, λs, μs, θ), all_augmented_vars
 end
 
 
@@ -616,7 +617,7 @@ x0 = [
 ###############################################################
 
 # Main body of algorithm implementation for hardware. Will restructure as needed.
-function nplayer_hierarchy_navigation(x0; verbose = false)
+function nplayer_hierarchy_navigation(x0; run_lq=false, verbose = false)
 	"""
 	Navigation function for a multi-player hierarchy game. Players are modeled as double integrators in 2D space, 
 		with objectives to reach certain sets of game states.
@@ -627,6 +628,7 @@ function nplayer_hierarchy_navigation(x0; verbose = false)
 	verbose (Bool, optional) : Whether to print verbose output (default: false).
 	"""
 
+	# TODO: Use the initial condition in the initial guess.
 	# Normalize x0 to Vector{Vector{Float64}} to support inputs from Python (which often pass a Matrix)
 	x0_vecs = if x0 isa AbstractMatrix
 		[collect(@view x0[i, :]) for i in 1:size(x0, 1)]
@@ -636,36 +638,29 @@ function nplayer_hierarchy_navigation(x0; verbose = false)
 		error("x0 must be a Matrix or a Vector of Vectors")
 	end
 
-	# Number of players in the game
-	N = 3
+	# Set up the problem.
+	T = 3
+	Δt = 0.5
+	N, G, H, problem_dims, Js, gs = get_three_player_openloop_lq_problem(T, Δt; verbose)
 
-	# Set up the information structure.
-	# This defines a stackelberg chain with three players, where P2 is the leader of P1 and P3, which
-	# are Nash with each other.
-	G = SimpleDiGraph(N);
-	add_edge!(G, 2, 1); # P2 -> P1
-	add_edge!(G, 2, 3); # P2 -> P3
+	primal_dimension_per_player = problem_dims.primal_dimension_per_player
+	state_dimension = problem_dims.state_dimension
+	control_dimension = problem_dims.control_dimension
 
-
-	H = 1
-	Hp1 = H+1 # number of planning stages is 1 for OL game.
-
-	# Helper function
-	flatten(vs) = collect(Iterators.flatten(vs))
-
-	# Initial sizing of various dimensions.
-	T = 10 # time horizon
-	Δt = 0.5 # time step
-	state_dimension = 2 # player 1,2,3's state dimension
-	control_dimension = 2 # player 1,2,3's control dimension
-
-	# Additional dimension computations.
-	x_dim = state_dimension * (T+1)
-	u_dim = control_dimension * (T+1)
-	aggre_state_dimension = x_dim * N
-	aggre_control_dimension = u_dim * N
-	total_dimension = aggre_state_dimension + aggre_control_dimension
-	primal_dimension_per_player = x_dim + u_dim
+	# Modify P2's objective function to be nonlinear unless we are running an LQ game.
+	if !run_lq
+		# Player 2's objective function: P2 wants P1 and P3 to get to the origin
+		function J₂_quartic(z₁, z₂, z₃, θ)
+			(; xs, us) = unflatten_trajectory(z₃, state_dimension, control_dimension)
+			xs³, us³ = xs, us
+			(; xs, us) = unflatten_trajectory(z₂, state_dimension, control_dimension)
+			xs², us² = xs, us
+			(; xs, us) = unflatten_trajectory(z₁, state_dimension, control_dimension)
+			xs¹, us¹ = xs, us
+			sum((0.5*(xs¹[end] .+ xs³[end])) .^ 4) + 0.05*sum(sum(u .^ 2) for u in us²)
+		end
+		Js[2] = J₂_quartic
+	end
 
 	# Print dimension information.
 	println("Number of players: $N")
@@ -673,74 +668,15 @@ function nplayer_hierarchy_navigation(x0; verbose = false)
 	println("Time Horizon (# steps): $T")
 	println("Step period: Δt = $(Δt)s")
 	println("Dimension per player: $(primal_dimension_per_player)")
-	println("Total primal dimension: $total_dimension")
+	println("Total primal dimension: $(problem_dims.total_dimension)")
 
-
-	#### Player Objectives ####
-	# Player 1's objective function: P1 wants to get close to P2's final position 
-	# considering only its own control effort.
-	function J₁(z₁, z₂, z₃, θ)
-		(; xs, us) = unflatten_trajectory(z₁, state_dimension, control_dimension)
-		xs¹, us¹ = xs, us
-		(; xs, us) = unflatten_trajectory(z₂, state_dimension, control_dimension)
-		xs², us² = xs, us
-		0.5*sum((xs¹[end] .- xs²[end]) .^ 2) + 0.05*sum(sum(u .^ 2) for u in us¹)
-	end
-
-	# Player 2's objective function: P2 wants P1 and P3 to get to the origin
-	function J₂(z₁, z₂, z₃, θ)
-		(; xs, us) = unflatten_trajectory(z₃, state_dimension, control_dimension)
-		xs³, us³ = xs, us
-		(; xs, us) = unflatten_trajectory(z₂, state_dimension, control_dimension)
-		xs², us² = xs, us
-		(; xs, us) = unflatten_trajectory(z₁, state_dimension, control_dimension)
-		xs¹, us¹ = xs, us
-		sum((0.5*(xs¹[end] .+ xs³[end])) .^ 2) + 0.05*sum(sum(u .^ 2) for u in us²)
-	end
-
-	# Player 3's objective function: P3 wants to get close to P2's final position considering its own and P2's control effort.
-	function J₃(z₁, z₂, z₃, θ)
-		(; xs, us) = unflatten_trajectory(z₃, state_dimension, control_dimension)
-		xs³, us³ = xs, us
-		(; xs, us) = unflatten_trajectory(z₂, state_dimension, control_dimension)
-		xs², us² = xs, us
-		0.5*sum((xs³[end] .- xs²[end]) .^ 2) + 0.05*sum(sum(u³ .^ 2) for u³ in us³) + 0.05*sum(sum(u² .^ 2) for u² in us²)
-	end
-
-	Js = Dict{Int, Any}(
-		1 => J₁,
-		2 => J₂,
-		3 => J₃,
-	)
-
-
-	#### player individual dynamics ####
-
-	# Dynamics are the only constraints (for now).
-	function dynamics(z, t)
-		(; xs, us) = unflatten_trajectory(z, state_dimension, control_dimension)
-		x = xs[t]
-		u = us[t]
-		xp1 = xs[t+1]
-		# rows 3:4 for p2 in A, and columns 3:4 for p2 in B when using the full stacked system
-		# but since A is I and B is block-diagonal by design, you can just write:
-		return xp1 - x - Δt*u
-	end
-
-	# Set up the equality constraints for each player.
-	make_ic_constraint(i) = function (zᵢ)
-		(; xs, us) = unflatten_trajectory(zᵢ, state_dimension, control_dimension)
-		x1 = xs[1]
-		return x1 - x0_vecs[i]
-	end
-
-	# In this game, each player has the same dynamics constraint.
-	dynamics_constraint(zᵢ) = mapreduce(vcat, 1:T) do t dynamics(zᵢ, t) end
-	gs = [function (zᵢ) vcat(dynamics_constraint(zᵢ), make_ic_constraint(i)(zᵢ)) end for i in 1:N]
-
+	# Solve the game using our non-LQ solver.
 	parameter_value = 1e-5
-	# z_sol_nonlq, status_nonlq, z_sol_lq, status_lq, info_nonlq, all_variables, vars, all_augmented_vars = compare_lq_and_nonlq_solver(H, G, primal_dimension_per_player, Js, gs; parameter_value, verbose)
-	z_sol_nonlq, status_nonlq, info_nonlq, all_variables, vars, all_augmented_vars = solve_nonlq_game_example(H, G, primal_dimension_per_player, Js, gs; parameter_value, verbose)
+	if run_lq
+		z_sol_nonlq, status_nonlq, z_sol_lq, status_lq, info_nonlq, info_lq, all_variables, vars, all_augmented_vars = compare_lq_and_nonlq_solver(H, G, primal_dimension_per_player, Js, gs; parameter_value, verbose)
+	else
+		z_sol_nonlq, status_nonlq, info_nonlq, all_variables, vars, all_augmented_vars = solve_nonlq_game_example(H, G, primal_dimension_per_player, Js, gs; parameter_value, verbose)
+	end
 	z_sol = z_sol_nonlq
 	(; πs, zs, λs, μs, θ) = vars
 	(; out_all_augment_variables, out_all_augmented_z_est) = all_augmented_vars
@@ -755,43 +691,41 @@ function nplayer_hierarchy_navigation(x0; verbose = false)
 	# TODO: Update this to work with the new formulation.
 	# Evaluate the KKT residuals at the solution to check solution quality.
 	z_sols = [z₁_sol, z₂_sol, z₃_sol]
+
+	# Evaluate the solution against the KKT conditions (or approximate KKT conditions for non-LQ).
+	if run_lq
+		evaluate_kkt_residuals(info_lq.πs, all_variables, z_sol_lq, θ, parameter_value; verbose = verbose)
+	end
 	evaluate_kkt_residuals(πs, out_all_augment_variables, out_all_augmented_z_est, θ, parameter_value; verbose = verbose)
-	# evaluate_kkt_residuals(πs, all_variables, z_sol, θ, parameter_value; verbose = verbose)
 
-	# Reconstruct trajectories from solutions
-	xs1, _ = unflatten_trajectory(z₁_sol, state_dimension, control_dimension)
-	xs2, _ = unflatten_trajectory(z₂_sol, state_dimension, control_dimension)
-	xs3, _ = unflatten_trajectory(z₃_sol, state_dimension, control_dimension)
 
-	# # Plot the trajectories.
-	# plot_player_trajectories(xs1, xs2, xs3, T, Δt, verbose)
+	# Plot the trajectories.
+	plot_player_trajectories(z_sols, T, Δt, problem_dims)
+
+	# Print solution information.
+	verbose && print_solution_info(z_sols, Js, problem_dims)
 
 	###################OUTPUT: next state, current control ######################
-	next_state = Vector{Vector{Float64}}()
-	curr_control = Vector{Vector{Float64}}()
-	(; xs, us) = unflatten_trajectory(z₁_sol, state_dimension, control_dimension)
-	push!(next_state, xs[2]) # next state of player 1
-	push!(curr_control, us[1]) # current control of player 1
-	if verbose
-		println("P1 (x,u) solution : ($xs, $us)")
-		println("P1 Objective: $(Js[1](z₁_sol, z₂_sol, z₃_sol, 0))")
-	end
-	(; xs, us) = unflatten_trajectory(z₂_sol, state_dimension, control_dimension)
-	push!(next_state, xs[2]) # next state of player 2
-	push!(curr_control, us[1]) # current control of player 2
-	if verbose
-		println("P2 (x,u) solution : ($xs, $us)")
-		println("P2 Objective: $(Js[2](z₁_sol, z₂_sol, z₃_sol, 0))")
-	end
-	(; xs, us) = unflatten_trajectory(z₃_sol, state_dimension, control_dimension)
-	push!(next_state, xs[2]) # next state of player 3
-	push!(curr_control, us[1]) # current control of player 3
-	if verbose
-		println("P3 (x,u) solution : ($xs, $us)")
-		println("P3 Objective: $(Js[3](z₁_sol, z₂_sol, z₃_sol, 0))")
-	end
+	next_state, curr_control = construct_output(z_sols, problem_dims)
 
 	return next_state, curr_control
 	# next_state: [ [x1_next], [x2_next], [x3_next] ] = [ [-0.0072, 1.7970], [1.7925, 3.5889], [5.4159, 7.2201] ] where xi_next = [ pⁱ_x, pⁱ_y]
 	# curr_control: [ [u1_curr], [u2_curr], [u3_curr] ] = [ [-0.0144, -0.4060], [-0.4150, -0.8222], [-1.1683, -1.5598] ] where ui_curr = [ vⁱ_x, vⁱ_y]
+end
+
+function construct_output(z_sols, problem_dims)
+	"""
+	Constructs the output format for the navigation function.
+	"""
+
+	next_state = Vector{Vector{Float64}}()
+	curr_control = Vector{Vector{Float64}}()
+
+	for (i, z_sol) in enumerate(z_sols)
+		(; xs, us) = unflatten_trajectory(z_sol, problem_dims.state_dimension, problem_dims.control_dimension)
+		push!(next_state, xs[2]) # next state of player i
+		push!(curr_control, us[1]) # current control of player i
+	end
+
+	return next_state, curr_control
 end
